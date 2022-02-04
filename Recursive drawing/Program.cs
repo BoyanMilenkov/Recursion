8ebeb862598fe876b5823dcd9b43612731e73cf6
BoyanMilenkov <80035234+BoyanMilenkov@users.noreply.github.com>
2022-02-04 10:53:00 +0200

    Add files via upload

---
+++ Recursive drawing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Recursive_drawing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Draw(5);
+        }
+        private static void Draw(int v)
+        {
+            if(v == 0)
+            {
+                return;
+            }
+            Console.WriteLine(new string('*', v));
+            Draw(v - 1);
+            Console.WriteLine(new string('#', v));
+        }
+    }
+}
